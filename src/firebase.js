860ddfbb1d194ae1546b5694dc1d1b0ef146b3dd
Nageshwar1997 <nageshpawarpatil@gmail.com>
2024-03-09 17:48:24 +0530

    Project Started

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClJfbUwHi4MEBh86q6WE_URJJyZcKyUwc",
+    authDomain: "fir-fc614.firebaseapp.com",
+    projectId: "fir-fc614",
+    storageBucket: "fir-fc614.appspot.com",
+    messagingSenderId: "272773704122",
+    appId: "1:272773704122:web:64c931d78239d5c19ad583",
+    measurementId: "G-ERKFKW5HYM"
+};
