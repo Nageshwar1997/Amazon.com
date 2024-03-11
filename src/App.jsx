4abf5538257958807c40ef52c3aa2722cf80f79b
Nageshwar1997 <nageshpawarpatil@gmail.com>
2024-03-11 19:44:43 +0530

    checkout Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import './App.css';
-import Header from './homePage/header/Header';
-import Home from './homePage/home/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./App.css";
+import Header from "./homePage/header/Header";
+import Home from "./homePage/home/Home";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
   return (
     // BEM
-    <div className="app">
-      {/* Header */}
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
