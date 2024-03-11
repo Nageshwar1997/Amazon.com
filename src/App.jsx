641344f7e44aac907638c7f2a72c1cc9f17776d6
Nageshwar1997 <nageshpawarpatil@gmail.com>
2024-03-11 18:35:39 +0530

    Product Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './homePage/header/Header';
+import Home from './homePage/home/Home';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
     <div className="app">
       {/* Header */}
       <Header />
+      <Home />
     </div>
   );
 }
